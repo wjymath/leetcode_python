75bf505954c6398aaebc38d623cdb0e7d968ea2f
wjymath <wjymath@foxmail.com>
2017-07-02 15:21:34 +0800

    2017/07/02 palindrome_number.py done

---
+++ 9.palindrome_number/palindrome_number.py
@@ -0,0 +1,11 @@
+class Solution(object):
+    def isPalindrome(self, x):
+        """
+        :type x: int
+        :rtype: bool
+        """
+        x = str(x)
+        return x == x[::-1]
+
+# a = Solution()
+# print a.isPalindrome(123321)
