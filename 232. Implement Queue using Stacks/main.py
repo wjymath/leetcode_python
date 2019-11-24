1d35a76af1729f3b178248909c9cc96022c7a37c
wjymath <wjymath@foxmail.com>
2019-11-24 23:13:38 +0800

    2019/11/24 栈的调用，list或者dqueue

--- 232. Implement Queue using Stacks/main.py
+++ 232. Implement Queue using Stacks/main.py
@@ -34,9 +34,3 @@ class MyQueue:
             return False
         else:
             return True
-# Your MyQueue object will be instantiated and called as such:
-# obj = MyQueue()
-# obj.push(x)
-# param_2 = obj.pop()
-# param_3 = obj.peek()
-# param_4 = obj.empty()
